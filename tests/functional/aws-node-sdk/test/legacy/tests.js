bbcb95305ee2f7c7a01a4ad935c9c522d4e283d1
Bennett Buchanan <bennett.buchanan@scality.com>
2016-11-28 14:15:33 -0800

    [squash me] Remove unnecessary checks

--- tests/functional/aws-node-sdk/test/legacy/tests.js
+++ tests/functional/aws-node-sdk/test/legacy/tests.js
@@ -24,7 +24,7 @@ const objectKey = 'toAbort&<>"\'';
 // same uploadId
 const multipartUploadData = {};
 
-describe('aws-node-sdk test suite as registered user', function testSuite() {
+    describe.only('aws-node-sdk test suite as registered user', function testSuite() {
     this.timeout(60000);
     let s3;
 
