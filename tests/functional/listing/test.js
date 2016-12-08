d8cfbc49593cb9db74f507040aa6a66b1f584849
Guillaume Gimenez <ploki@blackmilk.fr>
2016-12-07 17:19:48 -0800

    added listing corner case tests
    The validity of these tests may be verified against AWS S3 with the following
    commands
    ```
    AWS_ON_AIR=true npm run ft_test
    ```

--- tests/functional/listing/test.js
+++
@@ -1,8 +0,0 @@
-function useAws() {
-    return !!process.env['REAL_AWS'];
-}
-
-describe('test', () => {
-    if (useAws()) { /* awsconf */ console.log('aws######3') } else { /* scalconf */console.log('scal######3') }
-
-});
