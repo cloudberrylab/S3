67730a0f339e876b6a0377ccd76f200fa607ee6e
Lauren Spiegel <lhspiegel@gmail.com>
2016-12-08 17:22:33 -0800

    TO SQUASH  -- test fun

--- tests/functional/aws-node-sdk/test/object/websiteGet.js
+++ tests/functional/aws-node-sdk/test/object/websiteGet.js
@@ -1,20 +1,21 @@
 const http = require('http');
 const assert = require('assert');
 
+const S3 = require('aws-sdk').S3;
 const Browser = require('zombie');
 
 require('babel-core/register');
 const conf = require('../../../../../lib/Config').default;
+const getConfig = require('../support/config').default;
 const transport = conf.https ? 'https' : 'http';
-const hostname = 'bucketwebsitetester.s3-website-us-east-1.amazonaws.com';
+const bucket = 'bucketwebsitetester';
+const hostname = `${bucket}.s3-website-us-east-1.amazonaws.com`;
 
 const endpoint = `${transport}://${hostname}:8000`;
 
 // TODO: Add this endpoint in Integration for CI
-// TODO: Note in Docs that for testing need to add line to local etc/hosts:
-// 127.0.0.1 bucketwebsitetester.s3-website-us-east-1.amazonaws.com
 
-describe('User visits bucket website endpoint', () => {
+describe.only('User visits bucket website endpoint', () => {
     const browser = new Browser();
 
     it('should return 405 when user requests method other than get or head',
@@ -54,8 +55,31 @@ describe('User visits bucket website endpoint', () => {
     });
 
     describe('with existing bucket', () => {
-        before
-    })
+        let s3;
+        before(done => {
+            const config = getConfig('default', { signatureVersion: 'v4' });
+            s3 = new S3(config);
+            s3.createBucket({ Bucket: bucket }, err => done(err));
+        });
+
+        after(done => {
+            s3.deleteBucket({ Bucket: bucket }, err => done(err));
+        });
+
+        it('should return 404 when no website configuration', done => {
+            browser.visit(endpoint, () => {
+                browser.assert.status(404);
+                browser.assert.text('title', '404 Not Found');
+                browser.assert.text('h1', '404 Not Found');
+                browser.assert.text('#code',
+                    'Code: NoSuchWebsiteConfiguration');
+                browser.assert.text('#message',
+                    'Message: The specified bucket does not ' +
+                    'have a website configuration');
+                done();
+            });
+        });
+    });
 });
 
 // Tests:
@@ -63,7 +87,7 @@ describe('User visits bucket website endpoint', () => {
 // 2) website endpoint without a bucket name (would need separate etc/hosts
 // entry -- SKIP it)
 // 3) no such bucket X
-// 4) no website configuration
+// 4) no website configuration X
 // 5) no key in request -- uses index document
 // 6) path in request without key -- uses index document
 // 7) key is not public
